e39abc33711a864126820fe66c4693dee3c77002
Adolf Jurgens <adolffre@gmail.com>
2017-03-31 15:41:41 +0200

    teste 3

---
+++ Carthage/Build/iOS/Framework1.framework/Headers/Test1.swift
@@ -0,0 +1,16 @@
+//
+//  Test1.swift
+//  Framework1
+//
+//  Created by Adolf Jurgens Freitas on 3/30/17.
+//  Copyright © 2017 Adolf Jurgens Freitas. All rights reserved.
+//
+
+public class Test1 {
+    public init(){}
+    
+    public func test() -> String {
+        return "test1"
+    }
+    
+}
